0ae432cad43f274c9f9b86d73771bfce56074f82
Deus <amadeusrm97@gmail.com>
2022-04-04 20:56:45 -0400

    input created

---
+++ src/components/InputField.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+const InputField = () => {
+    return <form className='input'>
+        <input type='input' placeholder='Enter a Task' className='input_box'/>
+        <button className='input_submit' type='submit'> GO </button> 
+    </form>;
+};
+
+export default InputField
